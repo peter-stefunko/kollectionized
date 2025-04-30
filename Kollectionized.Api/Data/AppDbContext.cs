dea2e01e2c60a30033a714028b7c984645559147
peter-stefunko <peter.stefunko1@gmail.com>
2025-04-30 13:39:20 +0200

    update db context

--- Kollectionized.Api/Data/AppDbContext.cs
+++ Kollectionized.Api/Data/AppDbContext.cs
@@ -3,11 +3,9 @@ using Kollectionized.Api.Models;
 
 namespace Kollectionized.Api.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users => Set<User>();
-
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
