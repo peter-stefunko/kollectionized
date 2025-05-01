7c3153cccf5304f9cd41b1ad36cb72e6dc98b950
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-01 22:10:46 +0200

    update db context to be universal

--- Kollectionized.Api/Data/AppDbContext.cs
+++ Kollectionized.Api/Data/AppDbContext.cs
@@ -1,23 +1,69 @@
 using Microsoft.EntityFrameworkCore;
 using Kollectionized.Api.Models;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Kollectionized.Api.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users => Set<User>();
+    public DbSet<PokemonCollections> PokemonCollections => Set<PokemonCollections>();
+    public DbSet<PokemonDecks> PokemonDecks => Set<PokemonDecks>();
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>(entity =>
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
         {
-            entity.ToTable("users");
+            entity.SetTableName(ToSnakeCase(entity.GetTableName()!));
+            
+            foreach (var property in entity.GetProperties())
+            {
+                property.SetColumnName(ToSnakeCase(property.GetColumnName(StoreObjectIdentifier.Table(entity.GetTableName()!, null))!));
+            }
+            
+            foreach (var key in entity.GetKeys())
+            {
+                key.SetName(ToSnakeCase(key.GetName()!));
+            }
+            
+            foreach (var fk in entity.GetForeignKeys())
+            {
+                fk.SetConstraintName(ToSnakeCase(fk.GetConstraintName()!));
+            }
+            
+            foreach (var index in entity.GetIndexes())
+            {
+                index.SetDatabaseName(ToSnakeCase(index.GetDatabaseName()!));
+            }
+        }
 
-            entity.Property(u => u.Id).HasColumnName("id");
-            entity.Property(u => u.Username).HasColumnName("username");
-            entity.Property(u => u.PasswordHash).HasColumnName("password_hash");
-            entity.Property(u => u.CreatedAt).HasColumnName("created_at");
-        });
+        base.OnModelCreating(modelBuilder);
+    }
+
+    private static string ToSnakeCase(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return input;
+
+        var result = new System.Text.StringBuilder();
+        result.Append(char.ToLowerInvariant(input[0]));
+
+        for (int i = 1; i < input.Length; ++i)
+        {
+            var c = input[i];
+            if (char.IsUpper(c))
+            {
+                result.Append('_');
+                result.Append(char.ToLowerInvariant(c));
+            }
+            else
+            {
+                result.Append(c);
+            }
+        }
+
+        return result.ToString();
     }
 
 }
