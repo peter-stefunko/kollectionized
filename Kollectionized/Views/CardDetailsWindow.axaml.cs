a01d4f5693a78c1fec0dab2fb367605838d3896a
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-01 22:15:53 +0200

    add CardDetails window

---
+++ Kollectionized/Views/CardDetailsWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Kollectionized.Views;
+
+public partial class CardDetailsWindow : Window
+{
+    public CardDetailsWindow()
+    {
+        InitializeComponent();
+    }
+}
