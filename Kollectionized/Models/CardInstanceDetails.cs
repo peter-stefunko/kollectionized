06c3ab1b123e35ef2ecfbbf893b9005f6aa60afa
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-01 22:12:34 +0200

    add card instance model

---
+++ Kollectionized/Models/CardInstanceDetails.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+namespace Kollectionized.Models;
+
+public class CardInstanceDetails
+{
+    public Guid Id { get; set; }
+    public string OwnerUsername { get; set; } = string.Empty;
+    public decimal? Grade { get; set; }
+    public string GradingCompany { get; set; } = string.Empty;
+    public string Notes { get; set; } = string.Empty;
+    public List<string> TradeHistory { get; set; } = new();
+}
