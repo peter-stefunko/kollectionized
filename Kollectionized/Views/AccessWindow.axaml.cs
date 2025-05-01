9f1225d55129455686e5b7d882a7edc6c3608a4c
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-01 22:15:22 +0200

    add AccessWindow

---
+++ Kollectionized/Views/AccessWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Kollectionized.Views;
+
+public partial class AccessWindow : Window
+{
+    public AccessWindow()
+    {
+        InitializeComponent();
+    }
+}
