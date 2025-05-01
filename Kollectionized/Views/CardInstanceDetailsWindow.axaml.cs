ca05479121667378ef3df5ee1e489177124261f3
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-01 22:16:10 +0200

    add CardInstance window

---
+++ Kollectionized/Views/CardInstanceDetailsWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Kollectionized.Views;
+
+public partial class CardInstanceDetailsWindow : Window
+{
+    public CardInstanceDetailsWindow()
+    {
+        InitializeComponent();
+    }
+}
