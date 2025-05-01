803126900cd8f9c2b16c0fc6831b17a4ea4cb8b9
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-01 22:11:11 +0200

    update api user model

--- Kollectionized.Api/Models/User.cs
+++ Kollectionized.Api/Models/User.cs
@@ -1,18 +1,25 @@
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Kollectionized.Api.Models;
 
+[Table("users")]
 public class User
 {
     [Key]
+    [Column("id")]
     public Guid Id { get; set; } = Guid.NewGuid();
 
     [Required]
-    [StringLength(20, MinimumLength = 6)]
+    [Column("username")]
+    [MaxLength(20)]
     public string Username { get; set; } = string.Empty;
 
-    [Required]
-    public string PasswordHash { get; set; } = string.Empty;
+    [Column("password_hash")]
+    public string? PasswordHash { get; set; }
 
+    [Column("created_at")]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [Column("last_username")]
+    public string? LastUsername { get; set; }
 }
