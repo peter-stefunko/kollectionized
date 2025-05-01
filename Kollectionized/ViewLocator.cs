87855339a97dc3c5608190f0d7660d8aa66eea6f
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-01 22:08:55 +0200

    update ViewLocator with DataContext

--- Kollectionized/ViewLocator.cs
+++ Kollectionized/ViewLocator.cs
@@ -10,19 +10,21 @@ public class ViewLocator : IDataTemplate
     public Control? Build(object? param)
     {
         if (param is null)
-            return null;
+            return new TextBlock { Text = "Null parameter passed to ViewLocator." };
 
         var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
         var type = Type.GetType(name);
 
         if (type != null)
         {
-            return (Control)Activator.CreateInstance(type)!;
+            var control = (Control)Activator.CreateInstance(type)!;
+            control.DataContext = param;
+            return control;
         }
 
-        return new TextBlock { Text = "Not Found: " + name };
+        return new TextBlock { Text = $"View not found for: {param.GetType().FullName}" };
     }
-
+    
     public bool Match(object? data)
     {
         return data is ViewModelBase;
