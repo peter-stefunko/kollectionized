3f024c0433f8b4f92f6383872a4fc0015257a2b0
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-01 22:15:45 +0200

    add CardDetails view

---
+++ Kollectionized/Views/CardDetailsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Kollectionized.Views;
+
+public partial class CardDetailsView : UserControl
+{
+    public CardDetailsView()
+    {
+        InitializeComponent();
+    }
+}
