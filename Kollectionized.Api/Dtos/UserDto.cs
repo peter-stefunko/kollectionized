032ae5b1108d98775dd5102c126b7604fdf81ce1
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-08 20:28:28 +0200

    update user dto

--- Kollectionized.Api/Dtos/UserDto.cs
+++ Kollectionized.Api/Dtos/UserDto.cs
@@ -1,3 +1,3 @@
 namespace Kollectionized.Api.Dtos;
 
-public record UserDto(Guid Id, string Username);
+public record UserDto(Guid Id, string Username, DateTimeOffset CreatedAt, string LastUsername, string Bio);
