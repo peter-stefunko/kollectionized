9556664e6d7ceafd5ebf687805fd6051f577ec10
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-08 20:27:28 +0200

    add user controller

---
+++ Kollectionized.Api/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+using Kollectionized.Api.Data;
+using Kollectionized.Api.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kollectionized.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(AppDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
+    {
+        var users = await context.Users
+            .Where(u => !u.Username.StartsWith("[del-"))
+            .OrderBy(u => u.Username)
+            .Select(u => new UserDto(u.Id, u.Username, u.CreatedAt, u.LastUsername, u.Bio))
+            .ToListAsync();
+
+        return Ok(users);
+    }
+}
