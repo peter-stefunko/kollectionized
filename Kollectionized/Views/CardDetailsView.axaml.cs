55dacd71ef042719eeb2560adbfa927292d82375
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-08 20:35:37 +0200

    update card details view

--- Kollectionized/Views/CardDetailsView.axaml.cs
+++ Kollectionized/Views/CardDetailsView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Kollectionized.ViewModels;
 
 namespace Kollectionized.Views;
 
@@ -9,5 +10,6 @@ public partial class CardDetailsView : UserControl
     public CardDetailsView()
     {
         InitializeComponent();
+        /*DataContext = new CardDetailsViewModel();*/
     }
 }
