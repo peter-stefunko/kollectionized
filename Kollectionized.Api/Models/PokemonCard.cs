f33c5c45ebcae5f3568f9674a8540044fd3316c7
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-08 20:29:59 +0200

    add pokemon card model

---
+++ Kollectionized.Api/Models/PokemonCard.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kollectionized.Api.Models;
+
+[Table("pokemon_cards")]
+public class PokemonCard
+{
+    [Key]
+    public Guid Uuid { get; set; }
+
+    [Column("set")]
+    public string Set { get; set; } = string.Empty;
+    
+    [Column("card_number")]
+    public string CardNumber { get; set; } = string.Empty;
+    
+    [Column("name")]
+    public string Name { get; set; } = string.Empty;
+    
+    [Column("type")]
+    public string Type { get; set; } = string.Empty;
+    
+    [Column("form", TypeName = "jsonb")]
+    public string? Form { get; set; }
+    
+    [Column("image_url")]
+    public string ImageUrl { get; set; } = string.Empty;
+    
+    [Column("rarity")]
+    public string? Rarity { get; set; }
+    
+    [Column("pokedex_number")]
+    public int? PokedexNumber { get; set; }
+    
+    [Column("typings", TypeName = "jsonb")]
+    public string? Typings { get; set; }
+}
