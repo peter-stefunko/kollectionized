94e6b4995728598cf56f77280ad902a6c584361e
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-08 11:12:14 +0200

    make properties json objects

--- Kollectionized/Models/User.cs
+++ Kollectionized/Models/User.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace Kollectionized.Models;
 
 public class User
 {
+    [JsonPropertyName("id")]
     public Guid Id { get; set; }
+
+    [JsonPropertyName("username")]
     public string Username { get; set; } = string.Empty;
 }
