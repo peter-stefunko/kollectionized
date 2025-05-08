f043c4969a4cae2746caa10193076a65133764a6
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-08 20:30:24 +0200

    rename pokemon collection

--- Kollectionized.Api/Models/PokemonCollection.cs
+++ Kollectionized.Api/Models/PokemonCollection.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Kollectionized.Api.Models;
 
 [Table("pokemon_collections")]
-public class PokemonCollections
+public class PokemonCollection
 {
     [Key]
     public Guid Id { get; init; }
