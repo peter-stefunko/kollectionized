7e1c54195056e7f9f0b17639ff7d821670ef6e17
peter-stefunko <peter.stefunko1@gmail.com>
2025-05-09 10:32:49 +0200

    add list of constants

---
+++ Kollectionized/Common/Constants.cs
@@ -0,0 +1,21 @@
+namespace Kollectionized.Common;
+
+public static class Constants
+{
+    public const string DefaultApiBaseUrl = "https://kollectionized-api-delicate-field-1881.fly.dev/api/";
+
+    public static class Games
+    {
+        public const string Pokemon = "pokemon";
+    }
+
+    public static class Assets
+    {
+        public const string PokemonCardBack = "avares://Kollectionized/Assets/pokemon-card-back.jpg";
+    }
+
+    public static class Storage
+    {
+        public const string SupabaseBaseUrl = "https://luvbckqisrgzkrsckfqp.supabase.co/storage/v1/object/public";
+    }
+}
